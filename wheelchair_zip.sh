2408d9d4aaea8915ae14d322c015a6ce53c96af3
Simon Poole <simon@poole.ch>
2015-12-03 23:22:16 +0100

    Fixed issue with preset file not actually being included in zip

--- wheelchair_zip.sh
+++ wheelchair_zip.sh
@@ -4,7 +4,7 @@
 sed "s=ICONPATH:==" < wheelchair_master_preset.xml | sed "s/ICONTYPE/png/" > gen/wheelchair_zip_preset.xml
 cd gen
 rm  wheelchair.zip
-zip wheelchair.zip vespucci_zip_preset.xml
+zip wheelchair.zip wheelchair_zip_preset.xml
 cd ../icons/png
 ls *.png | zip -@ ../../gen/wheelchair.zip 
 #cd ../../i8n
