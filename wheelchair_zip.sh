9bc5fae9c69d6e799f94827835cb889da3944320
Simon Poole <simon@poole.ch>
2015-12-03 22:44:05 +0100

    Generated zip file and added download links to webpage

---
+++ wheelchair_zip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+sed "s=ICONPATH:==" < wheelchair_master_preset.xml | sed "s/ICONTYPE/png/" > gen/wheelchair_zip_preset.xml
+cd gen
+rm  wheelchair.zip
+zip wheelchair.zip vespucci_zip_preset.xml
+cd ../icons/png
+ls *.png | zip -@ ../../gen/wheelchair.zip 
+#cd ../../i8n
+#ls *.po | zip -@ ../gen/wheelchair.zip 
+
